1440797c4950b6d69e29c5dc641af4660cd9285b
kishan <guptakishan492@gmail.com>
2026-01-22 14:09:06 +0530

    going to prod

--- Dockerfile
+++ Dockerfile
@@ -23,4 +23,6 @@ COPY --from=build /app/target/*.jar app.jar
 
 EXPOSE 8080
 
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java","-jar","-Dspring.profiles.active=prod","app.jar"]
+
+# ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/smartbuddy/backend/config/SecurityConfig.java
+++ src/main/java/com/smartbuddy/backend/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://smartbuddy-frontend.vercel.app"));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
